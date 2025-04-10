54e60247bf31b836792d9ff891ba9c3274db6655
ayan.biswas@edgewrapper.com <ayan.biswas@edgewrapper.com>
2025-04-10 10:28:42 +0000

    Edited transactionType

--- client_code/transactionType/__init__.py
+++ client_code/transactionType/__init__.py
@@ -8,12 +8,25 @@ class transactionType(transactionTypeTemplate):
     # Set Form properties and Data Bindings.
      self.init_components(**properties)
      self.sidepanel_1.link_5.foreground = 'white'
-     org_data = anvil.server.call('get_transaction_groups')
+     org_data = [
+  {
+    'sl_no': 1,
+    'tran_name': 'Purchase',
+    'tran_short_name': 'PUR',
+    'tran_group_name': 'Sales',
+    'is_active': True,
+    # 'tran_subtypes': [
+    #   {'tran_subtype_id': 101, 'name': 'Local'},
+    #   {'tran_subtype_id': 102, 'name': 'International'},
+    # ]
+  }
+]
+
      self.group_grid.columns = [
        {"id": "sl_no", "title": "Sl no", "data_key": "sl_no"},
-       {"id": "org_id", "title": "Org id", "data_key": "org_id"},
-       {"id": "group_name", "title": "Group name", "data_key": "group_name"},
-       {"id": "group_short_name", "title": "Group short name", "data_key": "group_short_name"},
+       {"id": "tran_name", "title": "Transaction Name", "data_key": "tran_name"},
+       {"id": "tran_short_name", "title": "Transaction Short Name", "data_key": "tran_short_name"},
+       {"id": "tran_group_name", "title": "Transaction Short Name", "data_key": "tran_group_name"},
        {"id": "is_active", "title": "Active", "data_key": "is_active"},
        {"id": "cta", "title": "Action", "data_key": "cta"},
      ]
