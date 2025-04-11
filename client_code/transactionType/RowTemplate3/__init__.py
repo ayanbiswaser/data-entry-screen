504b83b78474ace7ef00c310e70ad4ede0c13bb7
ayan.biswas@edgewrapper.com <ayan.biswas@edgewrapper.com>
2025-04-11 07:58:19 +0000

    Edited transactionType and transactionType.RowTemplate3

--- client_code/transactionType/RowTemplate3/__init__.py
+++ client_code/transactionType/RowTemplate3/__init__.py
@@ -10,10 +10,10 @@ class RowTemplate3(RowTemplate3Template):
 
     # Any code you write here will run before the form opens.
 
-  def button_2_click(self, **event_args):
-    self.flow_panel_1.visible= not self.flow_panel_1.visible 
-    self.button_2.text='Close'
-    self.label_1.text=self.item['tran_subtypes']
+  # def button_2_click(self, **event_args):
+  #   self.flow_panel_1.visible= not self.flow_panel_1.visible 
+  #   self.button_2.text='Close'
+  #   self.label_1.text=self.item['tran_subtypes']
 
   def button_1_click(self, **event_args):
     open_form('transactionTypeForm',form_type="edit", form_header = "Edit Transaction Type", data=self.item)
