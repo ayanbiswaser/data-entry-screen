6b1119eb7121a4e60c9a2a356ddf88621487a3b3
ayan.biswas@edgewrapper.com <ayan.biswas@edgewrapper.com>
2025-04-11 06:50:13 +0000

    Edited transactionType

--- client_code/transactionType/__init__.py
+++ client_code/transactionType/__init__.py
@@ -8,20 +8,8 @@ class transactionType(transactionTypeTemplate):
     # Set Form properties and Data Bindings.
      self.init_components(**properties)
      self.sidepanel_1.link_5.foreground = 'white'
-     org_data = [
-  {
-    'created_at': '25-01-2025',
-    'org_id':'001',
-    'tran_name': 'Purchase',
-    'tran_short_name': 'PUR',
-    'tran_group_name': 'Sales',
-    'is_active': True,
-    'tran_subtypes': [
-      {'tran_subtype_id': 101, 'name': 'Local'},
-      {'tran_subtype_id': 102, 'name': 'International'},
-    ]
-  }
-]
+     data = anvil.server.call('get_transaction_types')
+
      self.data_grid_1.columns = [
        {"id": "created_at", "title": "created At", "data_key": "created_at"},
        {"id": "org_id", "title": "Org id", "data_key": "org_id"},
@@ -32,7 +20,7 @@ class transactionType(transactionTypeTemplate):
        {"id": "cta", "title": "Action", "data_key": "cta"},
        {"id": "view", "title": "Transaction Subtype", "data_key": "view"}
      ]
-     self.repeating_panel_1.items=org_data
+     self.repeating_panel_1.items=data
 
   def create_group_button_click(self, **event_args):
     open_form('transactionTypeForm',form_type="create", form_header = "Create Transaction")
