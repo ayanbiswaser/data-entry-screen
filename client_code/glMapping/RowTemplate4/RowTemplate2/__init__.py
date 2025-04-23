e9d4a7c7cb1fff8d1531a23a4f7ce5cecd43f457
ayan.biswas@edgewrapper.com <ayan.biswas@edgewrapper.com>
2025-04-23 16:12:25 +0000

    Edited glMapping.RowTemplate4.RowTemplate2 and glMapping.RowTemplate4

--- client_code/glMapping/RowTemplate4/RowTemplate2/__init__.py
+++ client_code/glMapping/RowTemplate4/RowTemplate2/__init__.py
@@ -7,5 +7,13 @@ class RowTemplate2(RowTemplate2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
+    self.drop_down_1.items = self.item['dropdown_1_items']
+    self.drop_down_2.items = self.item['dropdown_2_items']
+    self.drop_down_3.items = self.item['dropdown_3_items']
+
+    self.drop_down_1.selected_value = self.item['selected_1']
+    self.drop_down_2.selected_value = self.item['selected_2']
+    self.drop_down_3.selected_value = self.item['selected_3']
 
     # Any code you write here will run before the form opens.
--- client_code/glMapping/RowTemplate4/__init__.py
+++ client_code/glMapping/RowTemplate4/__init__.py
@@ -11,8 +11,43 @@ class RowTemplate4(RowTemplate4Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    self.link_1.text=
-    print()
+    self.link_1.text=self.item['map']
+    self.is_open=False
+
+#     self.drop_down_1.items = [
+#     {"text": "Select Org", "value": None},  # optional placeholder
+#     {"text": "Org A", "value": "org_a"},
+#     {"text": "Org B", "value": "org_b"},
+# ]
+
+    rows = [
+    {
+      "dropdown_1_items": ["A1", "A2", "A3"],
+      "dropdown_2_items": ["B1", "B2"],
+      "dropdown_3_items": ["C1", "C2", "C3", "C4"],
+      "selected_1": "A2",
+      "selected_2": "B1",
+      "selected_3": "C3"
+    },
+    {
+      "dropdown_1_items": ["A9", "A10"],
+      "dropdown_2_items": ["B5", "B6"],
+      "dropdown_3_items": ["C9"],
+      "selected_1": "A10",
+      "selected_2": "B6",
+      "selected_3": "C9"
+    }
+  ]
+
+    self.repeating_panel_1.items = rows
+
+    self.data_grid_1.columns = [
+      {"id": "mapping", "title": "MAPPING", "data_key": "mapping"},
+      {"id": "debit_account", "title": "DEBIT ACCOUNT", "data_key": "debit_account"},
+      {"id": "credit_account", "title": "CREDIT ACCOUNT", "data_key": "credit_account"},
+      {"id": "cta", "title": "Action", "data_key": "cta"},
+    ]
+
     # Any code you write here will run before the form opens.
 
 
@@ -34,3 +69,12 @@ class RowTemplate4(RowTemplate4Template):
 
   def link_1_click(self, **event_args):
     self.flow_panel_4.visible=not self.flow_panel_4.visible
+    self.is_open=not self.is_open
+    if self.is_open is True:
+      self.link_1.icon='fa:angle-up'
+    else:
+      self.link_1.icon='fa:angle-down'
+
+    
+    
+    
