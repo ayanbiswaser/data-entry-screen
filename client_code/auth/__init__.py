6989cf189301bf0efb9eda66b8583646815be11d
ayan.biswas@edgewrapper.com <ayan.biswas@edgewrapper.com>
2025-04-28 18:07:20 +0000

    Edited settings and auth

--- client_code/auth/__init__.py
+++ client_code/auth/__init__.py
@@ -2,24 +2,19 @@ from ._anvil_designer import authTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
-import anvil.users
-import anvil.server
-from time import sleep
-
-
-
 import plotly.graph_objects as go
+import anvil.server
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
 
 class auth(authTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    anvil.users.signup_with_form()
+    anvil.users.login_with_form()
+    user = anvil.users.get_user()
+    print(user['email'])
+
     # anvil.users.login_with_form()
