fbbe5c0a8f5724bbca0b654b720a415f4fbb4b90
ayan.biswas@edgewrapper.com <ayan.biswas@edgewrapper.com>
2025-04-29 02:22:28 +0000

    Edited sidepanel and auth; Deleted Form1

--- client_code/auth/__init__.py
+++ client_code/auth/__init__.py
@@ -13,8 +13,34 @@ class auth(authTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    anvil.users.login_with_form()
-    user = anvil.users.get_user()
-    print(user['email'])
+    self.find_user()
+    
 
-    # anvil.users.login_with_form()
+
+
+  def find_user(self):
+    user = anvil.users.get_user()
+    if user is not None:
+      self.login.visible=False
+      self.signup.visible=False
+      self.logout.visible=True
+
+    else:
+      self.login.visible=True
+      self.signup.visible=True
+      self.logout.visible=False
+  def signup_click(self, **event_args):
+    anvil.users.signup_with_form()
+    self.find_user()
+
+  def login_click(self, **event_args):
+    anvil.users.login_with_form()
+    self.find_user()
+
+  def logout_click(self, **event_args):
+    anvil.users.logout()
+    self.find_user()
+
+    
+    
+
