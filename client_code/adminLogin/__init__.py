8f54d4dc2b32bd7a7b362a163f56b45be989c964
ayan.biswas@edgewrapper.com <ayan.biswas@edgewrapper.com>
2025-04-30 06:32:01 +0000

    Edited adminLogin

--- client_code/adminLogin/__init__.py
+++ client_code/adminLogin/__init__.py
@@ -7,6 +7,8 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from anvil import Notification, Label
+
 
 
 class adminLogin(adminLoginTemplate):
@@ -19,5 +21,27 @@ class adminLogin(adminLoginTemplate):
 
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def link_2_click(self, **event_args):
+   try:
+        user = anvil.users.signup_with_google()
+        if user:
+          Notification(
+           f"{user['email']} you are login",
+           title="Success",
+           style="success"
+           ).show()
+        else:
+          Notification(
+           "Sign-up canceled.",
+           title="Success",
+           style="danger"
+           ).show()
+   except Exception as e:
+        alert(f"An error occurred: {e}")
 
 
